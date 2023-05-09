940e399fbe3c7ddeb87e74e0affa7dfc621cd0de
clarencetinator7 <clarencejoseph.dimafelix@gmail.com>
2023-05-09 20:14:37 +0800

    added TMPro + showing panel logic

--- Assets/Interaction.cs
+++ Assets/Interaction.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Interaction : MonoBehaviour, IInteractable
 {
+
+  [SerializeField] GameObject questionPanel;
+
   public void Interact()
   {
     Debug.Log("Interacting with " + gameObject.name);
+    questionPanel.SetActive(true);
   }
 }
