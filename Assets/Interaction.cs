624ce26b3c59072f61529fa01b979911fd889224
Clarence Joseph Dimafelix <clarencejoseph.dimafelix.s@bulsu.edu.ph>
2023-05-10 22:52:56 +0800

    improved question display logic

--- Assets/Interaction.cs
+++ Assets/Interaction.cs
@@ -7,43 +7,21 @@ using TMPro;
 public class Interaction : MonoBehaviour, IInteractable
 {
 
-  [SerializeField] GameObject questionPanel;
+  [SerializeField] GameObject questionManager;
 
-  // Question text mesh pro
-  [SerializeField] TextMeshProUGUI questionText;
-  [SerializeField] Button answerButton1;
-  [SerializeField] Button answerButton2;
-  [SerializeField] Button answerButton3;
+  [SerializeField] string questionText;
+  [SerializeField] string[] answers;
   [SerializeField] int correctAnswerIndex;
+
 
   public void Interact()
   {
     Debug.Log("Interacting with " + gameObject.name);
 
-    // Set question text
-    questionText.text = "What is the capital of France?";
+    // Create question
+    Question question = new Question(questionText, answers, correctAnswerIndex);
+    questionManager.GetComponent<QuestionManager>().showQuestion(question);
 
-    // Set answer text
-    answerButton1.GetComponentInChildren<TextMeshProUGUI>().text = "Paris";
-    answerButton2.GetComponentInChildren<TextMeshProUGUI>().text = "London";
-    answerButton3.GetComponentInChildren<TextMeshProUGUI>().text = "Berlin";
-
-    correctAnswerIndex = 0;
-
-
-    questionPanel.SetActive(true);
   }
 
-  public void AnswerQuestion(int answerIndex)
-  {
-    if (answerIndex == correctAnswerIndex)
-    {
-      Debug.Log("Correct!");
-    }
-    else
-    {
-      Debug.Log("Wrong!");
-    }
-    questionPanel.SetActive(false);
-  }
 }
--- Assets/QuestionManager.cs
+++ Assets/QuestionManager.cs
@@ -1,24 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Question
+{
+  public string questionText;
+  public string[] answers;
+  public int correctAnswerIndex;
+
+  public Question(string questionText, string[] answers, int correctAnswerIndex)
+  {
+    this.questionText = questionText;
+    this.answers = answers;
+    this.correctAnswerIndex = correctAnswerIndex;
+  }
+}
 
 public class QuestionManager : MonoBehaviour
 {
 
-  public void choiceOneClicked()
-  {
+  Question currentQuestion;
+  int correctAnswerIndex;
 
+  [SerializeField] GameObject questionPanel;
+  [SerializeField] TextMeshProUGUI questionText;
+  [SerializeField] Button answerButton1;
+  [SerializeField] Button answerButton2;
+  [SerializeField] Button answerButton3;
+
+  public void showQuestion(Question question)
+  {
+    currentQuestion = question;
+
+    // Set question text
+    questionPanel.transform.Find("QuestionText").GetComponent<TMPro.TextMeshProUGUI>().text = question.questionText;
+
+    // Set answer text
+    answerButton1.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = question.answers[0];
+    answerButton2.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = question.answers[1];
+    answerButton3.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = question.answers[2];
+
+    // Set correct answer index
+    correctAnswerIndex = question.correctAnswerIndex;
+
+    questionPanel.SetActive(true);
   }
 
-  public void choiceTwoClicked()
+  public void AnswerQuestion(int answerIndex)
   {
-
+    if (answerIndex == correctAnswerIndex)
+    {
+      Debug.Log("Correct!");
+      // Reward player
+    }
+    else
+    {
+      Debug.Log("Wrong!");
+      // Punish player
+    }
+    questionPanel.SetActive(false);
   }
-
-  public void choiceThreeClicked()
-  {
-
-  }
-
 
 }
