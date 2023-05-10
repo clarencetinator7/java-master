15cb002b9b4fed96dc59b3021eac7c0fda8afc55
Clarence Joseph Dimafelix <clarencejoseph.dimafelix.s@bulsu.edu.ph>
2023-05-10 22:27:28 +0800

    add display questions logic

--- Assets/Interaction.cs
+++ Assets/Interaction.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 
 public class Interaction : MonoBehaviour, IInteractable
 {
 
   [SerializeField] GameObject questionPanel;
 
+  // Question text mesh pro
+  [SerializeField] TextMeshProUGUI questionText;
+  [SerializeField] Button answerButton1;
+  [SerializeField] Button answerButton2;
+  [SerializeField] Button answerButton3;
+  [SerializeField] int correctAnswerIndex;
+
   public void Interact()
   {
     Debug.Log("Interacting with " + gameObject.name);
+
+    // Set question text
+    questionText.text = "What is the capital of France?";
+
+    // Set answer text
+    answerButton1.GetComponentInChildren<TextMeshProUGUI>().text = "Paris";
+    answerButton2.GetComponentInChildren<TextMeshProUGUI>().text = "London";
+    answerButton3.GetComponentInChildren<TextMeshProUGUI>().text = "Berlin";
+
+    correctAnswerIndex = 0;
+
+
     questionPanel.SetActive(true);
-    // Test change
+  }
+
+  public void AnswerQuestion(int answerIndex)
+  {
+    if (answerIndex == correctAnswerIndex)
+    {
+      Debug.Log("Correct!");
+    }
+    else
+    {
+      Debug.Log("Wrong!");
+    }
+    questionPanel.SetActive(false);
   }
 }
---
+++ Assets/QuestionManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionManager : MonoBehaviour
+{
+
+  public void choiceOneClicked()
+  {
+
+  }
+
+  public void choiceTwoClicked()
+  {
+
+  }
+
+  public void choiceThreeClicked()
+  {
+
+  }
+
+
+}
