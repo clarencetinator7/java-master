1290f00901e54da30ff5bbcc0c4d8988cae4d5e5
Clarence Joseph Dimafelix <clarencejoseph.dimafelix.s@bulsu.edu.ph>
2023-05-11 14:31:04 +0800

    display data chips ui

--- Assets/Scripts/ReadData.cs
+++ Assets/Scripts/ReadData.cs
@@ -7,11 +7,32 @@ public class ReadData : MonoBehaviour, IInteractable
 
   [SerializeField] string chipData = "This chip has no data.";
 
+  [SerializeField] GameObject dataPanel;
+
   public void Interact()
   {
     Debug.Log("Interacting with " + gameObject.name);
     // Read data from chip
-    Debug.Log(chipData);
+    readData(chipData);
+  }
+
+  public void readData(string data)
+  {
+    // Select the text from the child of dataPanel
+    dataPanel.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = data;
+    dataPanel.SetActive(true);
+
+    // Hide data panel after 5 seconds
+    StartCoroutine(HideDataPanel());
+
+  }
+
+  // Data panel coroutine will hide after 5 seconds
+  IEnumerator HideDataPanel()
+  {
+    yield return new WaitForSeconds(5);
+    dataPanel.SetActive(false);
+    Debug.Log("Data panel hidden");
   }
 
 }
