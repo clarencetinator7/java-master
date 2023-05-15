a0d34a5556ca1931335097049084451d6a945d5e
Clarence Joseph Dimafelix <clarencejoseph.dimafelix.s@bulsu.edu.ph>
2023-05-15 16:36:52 +0800

    add game manager

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+  public static GameManager instance;
+
+  void Awake()
+  {
+    MakeSingleton();
+  }
+
+  void MakeSingleton()
+  {
+    if (instance != null)
+    {
+      Destroy(gameObject);
+    }
+    else
+    {
+      instance = this;
+    }
+  }
+
+
+
+}
