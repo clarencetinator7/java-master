8c74ce6d4a059cac0de79d0e051fc91a1f4f651a
Clarence Joseph Dimafelix <clarencejoseph.dimafelix.s@bulsu.edu.ph>
2023-05-23 16:11:37 +0800

    add sound mute toggles

--- Assets/Scripts/Manager/SoundManager.cs
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class SoundManager : MonoBehaviour
 
   [SerializeField] private AudioSource _musicSource, _sfxSource;
 
+  [Header("Sounds Options")]
+  [SerializeField] private Button soundToggleBtn;
+  [SerializeField] private Sprite soundOn, soundOff;
   void Awake()
   {
     MakeSingleton();
@@ -29,5 +33,28 @@ public class SoundManager : MonoBehaviour
   public void playSound(AudioClip clip)
   {
     _sfxSource.PlayOneShot(clip);
+  }
+
+  public void toggleSounds()
+  {
+    if (!_musicSource.mute && !_sfxSource.mute)
+    {
+      _sfxSource.mute = true;
+      _musicSource.mute = true;
+      // change sprite to sound off
+      soundToggleBtn.GetComponent<Image>().sprite = soundOff;
+      // change color to #FF645D
+      soundToggleBtn.GetComponent<Image>().color = new Color32(255, 100, 93, 255);
+    }
+    else
+    {
+      _sfxSource.mute = false;
+      _musicSource.mute = false;
+      // change sprite to sound on
+      soundToggleBtn.GetComponent<Image>().sprite = soundOn;
+      // change color to #FF00FF
+      soundToggleBtn.GetComponent<Image>().color = new Color32(255, 0, 255, 255);
+
+    }
   }
 }
